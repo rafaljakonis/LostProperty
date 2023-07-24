7fcb0c5a58c8d24652661fabf0aaf32fe93a4437
Rafał Jakonis <rafaljakonis@gmail.com>
2023-07-24 20:26:34 +0200

    Initial Commit

---
+++ LostProperty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LostProperty
+//
+//  Created by Rafał Jakonis on 24/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
