67978f38fd8f9755c00a683ee210b3e90ee9b2cc
Rafał Jakonis <rafaljakonis@gmail.com>
2023-09-05 16:34:05 +0200

    Innit

--- LostProperty/ViewController.swift
+++ LostProperty/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        let myLabel = UILabel()
+        myLabel.text = "TEST"
+        myLabel.textAlignment = .center
+        myLabel.textColor = .cyan
+        myLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        
+        self.view.addSubview(myLabel)
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
